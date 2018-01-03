8a97187a265918312f7963dd53fdd74b40acbd49
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-01-03 17:01:04 -0500

    Changed ascii font for improved legibility

--- modules/ascii.js
+++ modules/ascii.js
@@ -3,12 +3,14 @@
 const chalk = require('chalk');
 
 // Pretty-print 'raagi' in green ascii art
+// http://patorjk.com/software/taag/#p=display&f=Straight&t=raagi
 const ascii = function() {
     console.log(
-        chalk.green(` _  _   _   _   o \n` +
-            `)  (_( (_( (_(  ( \n` +
-            `             _)   `)
+        chalk.green(` _  _   _   _  . \n` +
+            `|  (_| (_| (_) | \n` +
+            `           _/   `)
     );
 };
 
+// Export function as module
 module.exports = ascii;
