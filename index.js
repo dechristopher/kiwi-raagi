40ea96f95cd8363d7dead115acff550da587c9d2
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-01-10 11:46:39 -0500

    Reference changelog v1.4.0 for commit details

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const https = require('https');
 const request = require('request');
+const timeout = require('express-timeout-handler');
 const rcon = require('srcds-rcon');
 const fs = require('fs');
 
@@ -24,10 +25,26 @@ if (conf.ssl.enabled) {
 }
 
 // Inject bodyParser middleware to get request body
-//app.use(bodyParser.json()); // to support JSON-encoded bodies (currently disabled)
+app.use(bodyParser.json()); // to support JSON-encoded bodies (currently disabled)
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
+
+// Set up timeout middleware options
+var timeoutOptions = {
+    timeout: conf.timeout,
+    onTimeout: function(req, res) {
+        req.timedout = true;
+        res.status(503).send('Service unavailable. Please retry later. If this error persists, contact an engineer.');
+    },
+    onDelayedResponse: function(req, method, args, requestTime) {
+        console.log(`Attempted to call ${method} after timeout`);
+    },
+    disable: ['write', 'setHeaders', 'send', 'json', 'end']
+};
+
+// Ensure requests time out after 2 seconds
+app.use(timeout.handler(timeoutOptions));
 
 // Ensure all requests contain the auth token header:
 // KIWI-Raagi-Auth-Token
@@ -61,10 +78,10 @@ app.use(function(req, res, next) {
         if (sid > -1 && sid < conf.servers.length) {
             next();
         } else {
-            log('Bad request: ' + req.ip + ' -> invalid sid [' + sid + ']', undefined, true);
+            log(`Bad request: ${req.ip} -> invalid sid [${sid}]`, undefined, true);
             res.setHeader("Content-Type", "application/json");
             res.sendStatus(400);
-            res.send(`{"error":"invalid sid - ` + sid + `"`);
+            res.send(`{"error":"invalid sid - ${sid}"}`);
             res.end();
             return;
         }
@@ -90,10 +107,10 @@ app.use('/status/:sid', function(req, res, next) {
         if (sid > -1 && sid < conf.servers.length) {
             next();
         } else {
-            log('Bad request: ' + req.ip + ' -> invalid sid [' + sid + ']', undefined, true);
+            llog(`Bad request: ${req.ip} -> invalid sid [${sid}]`, undefined, true);
             res.setHeader("Content-Type", "application/json");
             res.sendStatus(400);
-            res.send(`{"error":"invalid sid - ` + sid + `"`);
+            res.send(`{"error":"invalid sid - ${sid}"}`);
             res.end();
             return;
         }
@@ -104,8 +121,8 @@ app.use('/status/:sid', function(req, res, next) {
 // Response Format (JSON) : {"version":"..."}
 app.get('/', (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    res.send(`{"version":"` + version + `"}`);
-    log('[GET /] (' + req.ip + ')', undefined, true);
+    res.send(`{"version":"${version}"}`);
+    log(`[GET /] ( ${req.ip} )`, undefined, true);
 });
 
 // Respond with calculated latency to server in ms
@@ -114,7 +131,7 @@ app.get('/', (req, res) => {
 app.post('/latency', (req, res) => {
     // Ensure client sends time
     if (req.body.time === undefined) {
-        log('[POST /latency] Bad request: ' + req.ip + ' -> time not provided', undefined, true);
+        log(`[POST /latency] Bad request: ( ${req.ip} ) -> time not provided`, undefined, true);
         res.setHeader("Content-Type", "application/json");
         res.sendStatus(400);
         res.send(`{"error":"time not provided"`);
@@ -132,7 +149,7 @@ app.post('/latency', (req, res) => {
     //console.log("Latency: " + latency);
     // Ensure client is not in the future
     if (latency < 0) {
-        log('[POST /latency] Bad request: ' + req.ip + ' -> invalid client time (future?)', undefined, true);
+        log(`[POST /latency] Bad request: ( ${req.ip} ) -> invalid client time (future?)`, undefined, true);
         res.setHeader("Content-Type", "application/json");
         res.sendStatus(400);
         res.send(`{"error":"invalid time (future?)"`);
@@ -141,14 +158,15 @@ app.post('/latency', (req, res) => {
     }
     // Send normal response
     res.setHeader("Content-Type", "application/json");
-    res.send(`{"latency":"` + latency + `"}`);
-    log('[POST /latency] (' + req.ip + ')', undefined, true);
+    res.send(`{"latency":"${latency}"}`);
+    log(`[POST /latency] ( ${req.ip} ) -> Latency: ${latency}ms`, undefined, true);
 });
 
 // Send command to server and reply with rcon command response:
 // Request Parameters:
 //	- sid (Body): server index from config.json [req.body.sid]
 //	- command (Body): command to run on remote server [req.body.command]
+// Response Code: (503) if server connection times out or is unreachable
 // Response Format (JSON) : {"command":"...", "output":"..."}
 app.post('/', (req, res) => {
     let conn = rcon({
@@ -157,23 +175,32 @@ app.post('/', (req, res) => {
     });
     conn.connect().then(() => {
         return conn.command(req.body.command).then((status) => {
+            if (req.timedout) {
+                //console.log('TIMED OUT');
+                con.disconnect();
+                return;
+            }
             res.setHeader("Content-Type", "application/json");
-            res.send(`{"command":"` + req.body.command + `", "output":"${status}"}`);
-            log('[POST /] (' + req.ip + ") -> [S: '" + conf.servers[req.body.sid].id + "'] [CMD: '" + req.body.command + "']", undefined, true);
+            res.send(`{"command":"${req.body.command}", "output":"${status}"}`);
+            log(`[POST /] ( ${req.ip} ) -> [S: '${conf.servers[req.body.sid].id}'] [CMD: '${req.body.command}']`, undefined, true);
         });
-    }).then(() => conn.disconnect());
-
-    //TODO (POST /): Build in error handling
+    }).then(() => {
+        conn.disconnect();
+        //console.log('DC');
+    }).catch((err) => {
+        console.log(`CAUGHT ERROR (RCON): ${err}`);
+    });
 });
 
 // Get current server status
 // Request Parameters:
 //	- sid (URL Param): server indexfrom config.json [req.params.sid]
+// Response Code: (503) if server connection times out or is unreachable
 // Response Format (JSON): {"sid": "...", "online": true, "players": x, "bots": x}
 app.get('/status/:sid', (req, res) => {
     // Build request options
     let options = {
-        url: conf.ssl.prefix + '://localhost:' + conf.port + '/',
+        url: `${conf.ssl.prefix}://localhost:${conf.port}/`,
         method: 'POST',
         headers: {
             'KIWI-Raagi-Auth-Token': conf.authtoken
@@ -202,7 +229,7 @@ app.get('/status/:sid', (req, res) => {
             respObject.players = Number(humans);
             respObject.bots = Number(bots);
             // Send response
-            log('[POST /pop/' + req.params.sid + '] (' + req.ip + ") -> [S: '" + conf.servers[req.params.sid].id + "'] [Population: '" + humans + "']", undefined, true);
+            log(`[POST /pop/${req.params.sid}] ( ${req.ip} ) -> [S: '${conf.servers[req.params.sid].id}'] [Population: '${humans}']`, undefined, true);
             res.setHeader('Content-Type', 'application/json');
             res.send(respObject);
         } else {
@@ -212,7 +239,7 @@ app.get('/status/:sid', (req, res) => {
             respObject.players = 0;
             respObject.bots = 0;
             // Send error response
-            log('[POST /pop/' + req.params.sid + '] (' + req.ip + ") -> [S: '" + conf.servers[req.params.sid].id + "'] ERROR: " + error, undefined, true);
+            log(`[POST /pop/${req.params.sid}] ( ${req.ip} ) -> [S: '${conf.servers[req.params.sid].id}'] ERROR: '${error}'`, undefined, true);
             res.setHeader('Content-Type', 'application/json');
             res.sendStatus(503);
             res.send(respObject);
@@ -228,11 +255,13 @@ if (conf.ssl.enabled) {
         cert: fs.readFileSync(conf.ssl.certfile)
     };
     // Listen for requests
-    https.createServer(options, app).listen(conf.port);
+    let srv = https.createServer(options, app).listen(conf.port, () => { /* Do stuff... */ });
+    srv.timeout = conf.timeout;
 } else {
     //No SSL
-    app.listen(conf.port, () => { /* Do stuff... */ });
+    let srv = app.listen(conf.port, () => { /* Do stuff... */ });
+    srv.timeout = conf.timeout;
 }
 
 ascii(); // Print ascii and startup information
-log('Init kiwi/raagi | v' + version + " | *:" + conf.port + " | SSL: " + conf.ssl.enabled, undefined, true);
+log(`Init kiwi/raagi | v${version} | *:${conf.port} | SSL: ${conf.ssl.enabled}`, undefined, true);
